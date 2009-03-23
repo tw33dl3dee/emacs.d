ea13605061417746dc8109bffb3a5e2a9425e08f
Ivan Korotkov <tw33dl3.dee@gmail.com>
2009-03-23 12:06:02 +0000

    * Added *DoxyMacs* support. * Disabled session due to multiple problems with it.
    git-svn-id: http://twee.cc/svn/emacs.d@5 85889587-2df5-4ba7-810f-77332f7d56c4

--- conf.d/90-session.el
+++ conf.d/90-session.el
@@ -12,63 +12,63 @@
 
 ;; Way 3: Desktop
 
-(desktop-save-mode 1)
+;;(desktop-save-mode 1)
 
-;; Customization follows below
-(setq history-length 250)
+;; ;; Customization follows below
+;; (setq history-length 250)
 
-(mapcar
- (lambda (symbol) 
-   (add-to-list 'desktop-globals-to-save symbol))
- '((buffer-name-history      . 100)
-   (dired-regexp-history     . 20)
-   (extended-command-history . 100)
-   (file-name-history        . 500)
-   (grep-history             . 50)
-   (minibuffer-history       . 100)
-   (query-replace-history    . 60)
-   (read-expression-history  . 60)
-   (regexp-history           . 60)
-   (regexp-search-ring       . 20)
-   (search-ring              . 20)
-   (shell-command-history    . 50)
-   desktop-dirname))
+;; (mapcar
+;;  (lambda (symbol) 
+;;    (add-to-list 'desktop-globals-to-save symbol))
+;;  '((buffer-name-history      . 100)
+;;    (dired-regexp-history     . 20)
+;;    (extended-command-history . 100)
+;;    (file-name-history        . 500)
+;;    (grep-history             . 50)
+;;    (minibuffer-history       . 100)
+;;    (query-replace-history    . 60)
+;;    (read-expression-history  . 60)
+;;    (regexp-history           . 60)
+;;    (regexp-search-ring       . 20)
+;;    (search-ring              . 20)
+;;    (shell-command-history    . 50)
+;;    desktop-dirname))
 
-(add-to-list 'desktop-locals-to-save 'buffer-file-coding-system)
-(add-to-list 'desktop-locals-to-save 'desktop-dirname)
+;; (add-to-list 'desktop-locals-to-save 'buffer-file-coding-system)
+;; (add-to-list 'desktop-locals-to-save 'desktop-dirname)
 
-(setq desktop-buffers-not-to-save
-	  (concat "\\(" "^nn\\.a[0-9]+\\|\\.log\\|(ftp)\\|^tags\\|^TAGS"
-			  "\\|\\.emacs.*\\|\\.diary\\|\\.newsrc-dribble\\|\\.bbdb" 
-			  "\\)$"))
-(add-to-list 'desktop-modes-not-to-save 'Info-mode)
-(add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
+;; (setq desktop-buffers-not-to-save
+;; 	  (concat "\\(" "^nn\\.a[0-9]+\\|\\.log\\|(ftp)\\|^tags\\|^TAGS"
+;; 			  "\\|\\.emacs.*\\|\\.diary\\|\\.newsrc-dribble\\|\\.bbdb" 
+;; 			  "\\)$"))
+;; (add-to-list 'desktop-modes-not-to-save 'Info-mode)
+;; (add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
 
-(setq-default desktop-load-locked-desktop nil)
-(setq-default desktop-missing-file-warning nil)
-(setq-default desktop-path (list "~/.emacs.d/"))
-(setq-default desktop-dirname "~/.emacs.d/")
-(setq-default desktop-save t)
-(setq-default desktop-save-mode t)
-(setq-default save-place t)
+;; (setq-default desktop-load-locked-desktop nil)
+;; (setq-default desktop-missing-file-warning nil)
+;; (setq-default desktop-path (list "~/.emacs.d/"))
+;; (setq-default desktop-dirname "~/.emacs.d/")
+;; (setq-default desktop-save t)
+;; (setq-default desktop-save-mode t)
+;; (setq-default save-place t)
 
-(defun my-desktop-ignore-semantic (desktop-buffer-file-name)
-       "Function to ignore cedet minor modes during restore of buffers"
-       nil)
-(add-to-list 'desktop-minor-mode-handlers '(semantic-stickyfunc-mode . my-desktop-ignore-semantic))
-(add-to-list 'desktop-minor-mode-handlers '(senator-minor-mode . my-desktop-ignore-semantic))
-(add-to-list 'desktop-minor-mode-handlers '(semantic-idle-scheduler-mode . my-desktop-ignore-semantic))
-(add-to-list 'desktop-minor-mode-handlers '(semantic-idle-summary-mode . my-desktop-ignore-semantic))
-(add-to-list 'desktop-minor-mode-handlers '(semantic-idle-completions-mode . my-desktop-ignore-semantic))
-(add-to-list 'desktop-minor-mode-handlers '(semantic-mru-bookmark-mode . my-desktop-ignore-semantic))
-(add-to-list 'desktop-minor-mode-handlers '(semantic-decoration-mode . my-desktop-ignore-semantic))
-(add-to-list 'desktop-minor-mode-handlers '(srecode-minor-mode . my-desktop-ignore-semantic))
-(add-to-list 'desktop-minor-mode-handlers '(ede-minor-mode . my-desktop-ignore-semantic))
+;; (defun my-desktop-ignore-semantic (desktop-buffer-file-name)
+;;        "Function to ignore cedet minor modes during restore of buffers"
+;;        nil)
+;; (add-to-list 'desktop-minor-mode-handlers '(semantic-stickyfunc-mode . my-desktop-ignore-semantic))
+;; (add-to-list 'desktop-minor-mode-handlers '(senator-minor-mode . my-desktop-ignore-semantic))
+;; (add-to-list 'desktop-minor-mode-handlers '(semantic-idle-scheduler-mode . my-desktop-ignore-semantic))
+;; (add-to-list 'desktop-minor-mode-handlers '(semantic-idle-summary-mode . my-desktop-ignore-semantic))
+;; (add-to-list 'desktop-minor-mode-handlers '(semantic-idle-completions-mode . my-desktop-ignore-semantic))
+;; (add-to-list 'desktop-minor-mode-handlers '(semantic-mru-bookmark-mode . my-desktop-ignore-semantic))
+;; (add-to-list 'desktop-minor-mode-handlers '(semantic-decoration-mode . my-desktop-ignore-semantic))
+;; (add-to-list 'desktop-minor-mode-handlers '(srecode-minor-mode . my-desktop-ignore-semantic))
+;; (add-to-list 'desktop-minor-mode-handlers '(ede-minor-mode . my-desktop-ignore-semantic))
 
-(add-hook 'desktop-after-read-hook #'(lambda () (setq desktop-dirname "~/.emacs.d/") (setq-default desktop-dirname "~/.emacs.d/") (prin1 desktop-dirname)))
-(add-hook 'desktop-after-read-hook #'(lambda () (interactive) (list-buffers) (switch-to-buffer "*Buffer List*") (delete-other-windows)))
+;; (add-hook 'desktop-after-read-hook #'(lambda () (setq desktop-dirname "~/.emacs.d/") (setq-default desktop-dirname "~/.emacs.d/") (prin1 desktop-dirname)))
+;; (add-hook 'desktop-after-read-hook #'(lambda () (interactive) (list-buffers) (switch-to-buffer "*Buffer List*") (delete-other-windows)))
 
-(progn (interactive) (desktop-read "~/.emacs.d/") (setq desktop-dirname "~/.emacs.d/"))
+;; (progn (interactive) (desktop-read "~/.emacs.d/") (setq desktop-dirname "~/.emacs.d/"))
 
-(setq desktop-dirname "~/.emacs.d/")
-(setq-default desktop-dirname "~/.emacs.d/")
+;; (setq desktop-dirname "~/.emacs.d/")
+;; (setq-default desktop-dirname "~/.emacs.d/")
