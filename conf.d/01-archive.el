ae23be2106388884012d5c8161caf155809e0b27
Ivan Korotkov <tw33dl3.dee@gmail.com>
2009-04-13 15:34:38 +0000

    Added several archive extensions.
    git-svn-id: http://twee.cc/svn/emacs.d@10 85889587-2df5-4ba7-810f-77332f7d56c4

---
+++ conf.d/01-archive.el
@@ -0,0 +1,6 @@
+;;
+;; Extension list for Archive Mode
+;;
+
+(add-to-list 'auto-mode-alist '("\\.\\(skz\\|pak\\|pk\\|pk3\\|pk4\\|SKZ\\|PAK\\|PK\\|PK3\\|PK4\\)$" . archive-mode))
+(add-to-list 'auto-coding-alist '("\\.\\(skz\\|pak\\|pk\\|pk3\\|pk4\\|SKZ\\|PAK\\|PK\\|PK3\\|PK4\\)$" . no-conversion))
