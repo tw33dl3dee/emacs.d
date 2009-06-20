5155ef24d8728600681d755bf5ff84d8f4962e03
Ivan Korotkov <tw33dl3.dee@gmail.com>
2009-06-20 10:54:47 +0000

    *Matlab* mode added.
    git-svn-id: http://twee.cc/svn/emacs.d@20 85889587-2df5-4ba7-810f-77332f7d56c4

--- init.el
+++ init.el
@@ -109,3 +109,10 @@
   (if (file-readable-p path) (load-file path)))
 
 (load-file-silently "~/.emacs.d/local.el")
+
+ (autoload 'matlab-mode "matlab" "Matlab Editing Mode" t)
+ (add-to-list
+  'auto-mode-alist
+  '("\\.m$" . matlab-mode))
+ (setq matlab-indent-function t)
+ (setq matlab-shell-command "matlab")
