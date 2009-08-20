e6b706b6eaf1e55cf2facfaf67fdb345d8e7b7ca
Ivan Korotkov <tw33dl3.dee@gmail.com>
2009-08-20 09:36:47 +0000

    * Added @APEL@ and @FLIM@ libraries needed for some *W3m* features. * Byte-compiled missing *W3m* files.
    git-svn-id: http://twee.cc/svn/emacs.d@40 85889587-2df5-4ba7-810f-77332f7d56c4

---
+++ conf.d/00-load-paths.el
@@ -0,0 +1,8 @@
+; global load paths required by later commands
+
+; APE library
+(add-to-list 'load-path "~/.emacs.d/site-lisp/apel")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/emu")
+
+; FLIM library
+(add-to-list 'load-path "~/.emacs.d/site-lisp/flim")
