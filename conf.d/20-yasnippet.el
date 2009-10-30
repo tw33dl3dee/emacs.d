7e1e7ca76adc75876c2ee99fe3d82c0d73ac9e22
Ivan Korotkov <tw33dl3.dee@gmail.com>
2009-10-30 12:14:05 +0000

    *Yasnippet* mode added.
    git-svn-id: http://twee.cc/svn/emacs.d@45 85889587-2df5-4ba7-810f-77332f7d56c4

---
+++ conf.d/20-yasnippet.el
@@ -0,0 +1,6 @@
+;; Yasnippet mode
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/yasnippet")
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/site-lisp/yasnippet/snippets")
