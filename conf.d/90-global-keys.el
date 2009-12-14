dfb4aabe58827117d25fd4443de7bd584272798f
Ivan Korotkov <tw33dl3.dee@gmail.com>
2009-12-14 13:06:52 +0000

    * *CPerl* enhancements. * Added global key @F5@ to find buffer by name at point.
    git-svn-id: http://twee.cc/svn/emacs.d@52 85889587-2df5-4ba7-810f-77332f7d56c4

--- conf.d/90-global-keys.el
+++ conf.d/90-global-keys.el
@@ -15,6 +15,7 @@
 (global-set-key "\C-z\C-x" (lambda () (interactive) (compile-and-run)))
 (global-set-key [(f1)] (lambda () (interactive) (manual-entry (current-word))))
 (global-set-key [(f2)] 'ffap-perl-module)
+(global-set-key [(f5)] (lambda () (interactive) (switch-to-buffer (find-buffer-visiting (current-word)))))
 (global-set-key "\C-z\C-r" (lambda () (interactive) (revert-buffer nil t)))
 (global-set-key (kbd "C-=") 'lisp-match-paren)
 
