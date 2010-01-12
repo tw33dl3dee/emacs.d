d77c3c228e57ade16e5ab31aaf02f0293ed2a486
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-01-12 16:19:52 +0000

    Added *LaTeX* support with various sugars: @AuCTeX@, @RefTeX@, etc.
    git-svn-id: http://twee.cc/svn/emacs.d@73 85889587-2df5-4ba7-810f-77332f7d56c4

--- init.el
+++ init.el
@@ -11,6 +11,11 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(PC-include-file-path (quote ("/usr/include" "/usr/local/include")))
+ '(TeX-auto-save t)
+ '(TeX-close-quote ">>")
+ '(TeX-open-quote "<<")
+ '(TeX-output-view-style (quote (("^dvi$" ("^landscape$" "^pstricks$\\|^pst-\\|^psfrag$") "%(o?)dvips -t landscape %d -o && gv %f") ("^dvi$" "^pstricks$\\|^pst-\\|^psfrag$" "%(o?)dvips %d -o && gv %f") ("^dvi$" ("^\\(?:a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4\\)$" "^landscape$") "%(o?)xdvi %dS -paper a4r -s 0 %d") ("^dvi$" "^\\(?:a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4\\)$" "%(o?)xdvi %dS -paper a4 %d") ("^dvi$" ("^\\(?:a5\\(?:comb\\|paper\\)\\)$" "^landscape$") "%(o?)xdvi %dS -paper a5r -s 0 %d") ("^dvi$" "^\\(?:a5\\(?:comb\\|paper\\)\\)$" "%(o?)xdvi %dS -paper a5 %d") ("^dvi$" "^b5paper$" "%(o?)xdvi %dS -paper b5 %d") ("^dvi$" "^letterpaper$" "%(o?)xdvi %dS -paper us %d") ("^dvi$" "^legalpaper$" "%(o?)xdvi %dS -paper legal %d") ("^dvi$" "^executivepaper$" "%(o?)xdvi %dS -paper 7.25x10.5in %d") ("^dvi$" "." "%(o?)xdvi %dS %d") ("^pdf$" "." "kpdf %o") ("^html?$" "." "netscape %o"))))
+ '(TeX-parse-self t)
  '(ange-ftp-try-passive-mode t)
  '(auto-compression-mode t nil (jka-compr))
  '(auto-image-file-mode t)
@@ -114,6 +119,8 @@
  '(tab-stop-list (quote (4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 80 88 96 104 104 112)))
  '(tab-width 4)
  '(tags-revert-without-query t)
+ '(tex-close-quote ">>")
+ '(tex-open-quote "<<")
  '(tramp-default-method "ssh")
  '(tramp-verbose 4)
  '(transient-mark-mode t)
