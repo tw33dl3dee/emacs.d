7da889b4599f5ed628c52a3143c192828e59723e
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-02-19 20:32:07 +0000

    Added script to benchmark *Emacs* loading time.
    git-svn-id: http://twee.cc/svn/emacs.d@93 85889587-2df5-4ba7-810f-77332f7d56c4

---
+++ benchmark.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+REV=`svnversion`
+TIME=`which time`
+
+[[ -z "$REV" ]] || [[ -z "$TIME" ]] && exit 1
+
+(
+	printf "%-8s " $REV
+	$TIME -f%E emacs -eval '(kill-emacs)' 2>&1
+) >> bench.log
+
+tail -n3 bench.log
