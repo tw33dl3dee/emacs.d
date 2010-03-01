560eb8fdc373a24f97ab77bbed74f4bf7cc48aa2
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-03-01 14:17:05 +0000

    Added language environment for Windows-1251 encoding.
    git-svn-id: http://twee.cc/svn/emacs.d@107 85889587-2df5-4ba7-810f-77332f7d56c4

---
+++ conf.d/20-win1251.el
@@ -0,0 +1,12 @@
+;;; Add Windows-1251 language environment
+
+(set-language-info-alist
+ "Cyrillic-CP1251" `((charset cyrillic-iso8859-5)
+		     (coding-system cp1251)
+		     (coding-priority cp1251)
+		     (input-method . "cyrillic-jcuken")
+		     (features cyril-util)
+		     (unibyte-display . cp1251)
+		     (sample-text . "Russian (Русский)    Здравствуйте!")
+		     (documentation . "Support for Cyrillic CP1251."))
+ '("Cyrillic"))
