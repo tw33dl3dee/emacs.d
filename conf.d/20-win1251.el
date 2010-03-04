23f8eb9fea381c1162d0db7e315336ae667204eb
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-03-04 10:28:09 +0000

    Fixed input system for Window-1251 language environment.
    git-svn-id: http://twee.cc/svn/emacs.d@109 85889587-2df5-4ba7-810f-77332f7d56c4

--- conf.d/20-win1251.el
+++ conf.d/20-win1251.el
@@ -4,7 +4,7 @@
  "Cyrillic-CP1251" `((charset cyrillic-iso8859-5)
 		     (coding-system cp1251)
 		     (coding-priority cp1251)
-		     (input-method . "cyrillic-jcuken")
+		     (input-method . "russian-computer")
 		     (features cyril-util)
 		     (unibyte-display . cp1251)
 		     (sample-text . "Russian (Русский)    Здравствуйте!")
