1b47e556e3579c59debcb39d2d5f7e3dc1b3ff08
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-03-04 10:30:00 +0000

    Fixes #38: @vc-dir@ buffer jumps to top-level directory for SCMs like git and Bazaar.
    git-svn-id: http://twee.cc/svn/emacs.d@110 85889587-2df5-4ba7-810f-77332f7d56c4

--- conf.d/20-dvc.el
+++ conf.d/20-dvc.el
@@ -1,4 +1,4 @@
-;;; DVC mode
+;;; Various enhancements for DVCSs (dvc-mode, custom commands, etc)
 ;;;
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp/dvc")
@@ -7,3 +7,12 @@
 (defun my-git-whatsnew ()
   (interactive) 
   (xgit-changelog "ORIG_HEAD" "HEAD" t))
+
+;;; When opening VC status on vc-controlled subdirectory, jump to the top directory
+;;; This makes all paths correct
+(defadvice vc-dir-prepare-status-buffer (before my-vcs-goto-top-directory activate compile)
+  (let* ((backend (ad-get-arg 2))
+	 (vcs-dir (ad-get-arg 1))
+	 (vcs-top-dir (vc-call-backend backend 'responsible-p vcs-dir)))
+    (when (stringp vcs-top-dir)
+      (ad-set-arg 1 vcs-top-dir))))
