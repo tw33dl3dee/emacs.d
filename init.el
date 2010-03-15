6fefbd010f6a88049f286a41d983d193edc353ec
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-03-15 13:06:19 +0000

    Fixed order of autoloads and Custom evaluation.
    Previously, Custom was executed before autoloads which resulted in autoloaded @defcustom@'s being not set.
    
    
    git-svn-id: http://twee.cc/svn/emacs.d@144 85889587-2df5-4ba7-810f-77332f7d56c4

--- init.el
+++ init.el
@@ -3,14 +3,14 @@
 ;;; Init load paths
 (load-file "~/.emacs.d/base.el")
 
-(setq custom-file (user-path "custom.el"))
-(load custom-file 'noerror)
-
 ;; Redefined in base.el
 (load-file-silently generated-autoload-file
 		    (lambda ()
 		      (error "You need to run `make' in %s (%s is missing)" 
 			     emacs-user-root generated-autoload-file)))
+
+(setq custom-file (user-path "custom.el"))
+(load custom-file 'noerror)
 
 ;; First try to load configuration from cached conf.el
 ;; Fallback to reading conf.d/*.el
