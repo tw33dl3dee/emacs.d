861d43ce52331ae9a4836b2e94c397ec567129b2
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-03-18 10:55:30 +0000

    *Org* mode: unbound @S-left/right@ hotkeys.
    git-svn-id: http://twee.cc/svn/emacs.d@148 85889587-2df5-4ba7-810f-77332f7d56c4

--- conf.d/01-org.el
+++ conf.d/01-org.el
@@ -1,8 +1,11 @@
 ;;; ORG-mode customization
 
 ;;; Turn on visual line wrapping automatically
+;;; Remove S-right/left bindings as they collide with ElScreen
 (add-hook 'org-mode-hook
 	  '(lambda ()
 	     (interactive)
+	     (local-unset-key (kbd "<S-left>"))
+	     (local-unset-key (kbd "<S-right>"))
 	     (unless visual-line-mode
 	       (visual-line-mode))))
