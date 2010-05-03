5196f94c37fc346da365eb9e0d49e84f9562ca00
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-05-03 11:22:50 +0000

    Disabled Scroll Lock flashing on bell (too buggy, eats CPU when in tty).
    git-svn-id: http://twee.cc/svn/emacs.d@173 85889587-2df5-4ba7-810f-77332f7d56c4

--- conf.d/04-scroll-lock.el
+++ conf.d/04-scroll-lock.el
@@ -1,8 +1,9 @@
 ;;; Misc stuff around Scroll Lock
 
+;; Nice but produces lots of bugs and hangs in tty
 ;; Flash Scroll Lock LED instead of ringing bell
-(setq ring-bell-function (lambda ()
-			   (call-process-shell-command "xset led 3; xset -led 3" nil 0 nil)))
+;(setq ring-bell-function (lambda ()
+;			   (call-process-shell-command "xset led 3; xset -led 3" nil 0 nil)))
 
 ;; Pressing Scroll Lock locks cursor position and lets text scroll around it
 (global-set-key (kbd "<Scroll_Lock>") 'scroll-lock-mode)
