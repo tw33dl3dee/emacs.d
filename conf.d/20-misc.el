5695cc055307ffbce5c677881047a8d3c071c65e
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-05-17 14:52:51 +0000

    Alternative open-line (@C-o/M-o@) behaviour.
    Also, in addition to C-w, M-w operates on current line when no mark, too.
    
    
    git-svn-id: http://twee.cc/svn/emacs.d@180 85889587-2df5-4ba7-810f-77332f7d56c4

--- conf.d/20-misc.el
+++ conf.d/20-misc.el
@@ -18,6 +18,15 @@
 ;; Use M-<right/left...> to switch between windows
 (windmove-default-keybindings 'meta)
 
+;; M-w copies current line when no mark
+(defadvice kill-ring-save (before slick-copy activate compile)
+      "When called interactively with no active region, copy a single line instead."
+      (interactive
+       (if mark-active (list (region-beginning) (region-end))
+         (message "Copied line")
+         (list (line-beginning-position)
+               (line-beginning-position 2)))))
+
 ;; C-w kills current line when no mark
 (defadvice kill-region (before slick-cut activate compile)
   "When called interactively with no active region, kill a single line instead."
@@ -45,3 +54,29 @@
     (abort-recursive-edit)))
 
 (add-hook 'mouse-leave-buffer-hook 'stop-using-minibuffer)
+
+;; Alternative C-o M-o behaviour
+
+(defvar newline-and-indent t
+  "Modify the behavior of the open-*-line functions to cause them to autoindent.")
+
+(defun open-next-line (arg)
+  "Move to the next line and then opens a line.
+    See also `newline-and-indent'."
+  (interactive "p")
+  (end-of-line)
+  (open-line arg)
+  (next-line 1)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+(global-set-key (kbd "C-o") 'open-next-line)
+
+(defun open-previous-line (arg)
+  "Open a new line before the current one. 
+     See also `newline-and-indent'."
+  (interactive "p")
+  (beginning-of-line)
+  (open-line arg)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+(global-set-key (kbd "M-o") 'open-previous-line)
