e419898066f071f2cb619076009608d7efbfeb10
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-05-22 21:01:12 +0000

    *Dia* files editing support (auto compression).
    git-svn-id: http://twee.cc/svn/emacs.d@183 85889587-2df5-4ba7-810f-77332f7d56c4

--- conf.d/01-archive.el
+++ conf.d/01-archive.el
@@ -5,9 +5,20 @@
 (add-to-list 'auto-mode-alist '("\\.\\(skz\\|pak\\|pk\\|pk3\\|pk4\\|SKZ\\|PAK\\|PK\\|PK3\\|PK4\\)$" . archive-mode))
 (add-to-list 'auto-coding-alist '("\\.\\(skz\\|pak\\|pk\\|pk3\\|pk4\\|SKZ\\|PAK\\|PK\\|PK3\\|PK4\\)$" . no-conversion))
 
-;; (add-to-list' jka-compr-compression-info-list ["\\.txz\\(~\\|\\.~[0-9]+~\\)?\\'"
-;; 					       "compressing"        "gzip"         ("-c" "-q")
-;; 					       "uncompressing"      "gzip"         ("-c" "-q" "-d")
-;; 					       t nil "\037\213"])
+(add-to-list 'auto-mode-alist '("\\.\\(dia\\|DIA\\)$" . archive-mode))
+(add-to-list 'auto-coding-alist '("\\.\\(dia\\|DIA\\)$" . no-conversion))
 
-;;(add-to-list 'jka-compr-mode-alist-additions (cons "\\.txz\\'" 'tar-mode))
+(eval-after-load "jka-compr"
+      '(unless (member "\\.\\(dia\\|DIA\\)\\'" (mapcar (lambda (vec) (elt vec 0))
+						       jka-compr-compression-info-list))
+         (add-to-list 'jka-compr-compression-info-list
+                      ;; this element from emacs 22
+                      ["\\.\\(dia\\|DIA\\)\\'"
+                       nil              nil     nil
+                       "uncompressing"  "gzip"  ("-c" "-q" "-d")
+                       nil t "\037\213"])
+         ;; if already enabled then toggle to get our addition recognised (note
+         ;; no `auto-compression-mode' variable in xemacs 21)
+         (when jka-compr-added-to-file-coding-system-alist
+           (auto-compression-mode 0)
+           (auto-compression-mode 1))))
