a78af719c7f01e47408c476d869eeaa83f76e6d8
Ivan Korotkov <tw33dl3.dee@gmail.com>
2010-12-09 16:53:14 +0000

    Added *Java* style fix for annotations and and generics.
    git-svn-id: http://twee.cc/svn/emacs.d@207 85889587-2df5-4ba7-810f-77332f7d56c4

---
+++ site-lisp/java5-font-lock.el
@@ -0,0 +1,128 @@
+;;; java5-font-lock.el --- Add Java 5'ism font-locking.
+
+;;; Commentary:
+;; Adds java5 font-locking (generics, for-in oops, annotations, enum, var-args)
+;; support to JDE's existing java font-lock support.
+
+;;; History:
+;; 2007/01/31 v0.1 initial version
+
+;; TODO remove hard-coded colors and replace with customizable faces
+
+;;; Code:
+;;;###autoload
+(defun ami-jde-font-lock-java5 ()
+  "Add font-lock keywords for Java 1.5'isms.
+http://java.sun.com/j2se/1.5.0/docs/guide/language/"
+  (interactive)
+  (let* ((id "[A-Za-z$_0-9]")
+         (constant "\\_<[A-Z][A-Z$_0-9]\\{1,\\}\\_>")
+         (cap-word (concat "\\_<[[:upper:]]" id "*\\_>"))
+         (low-word (concat "\\_<[[:lower:]]" id "*\\_>"))
+         (annotation (concat "@" cap-word))
+         (avoid-faces '(font-lock-comment-face
+                        font-lock-doc-face font-lock-string-face)))
+    (font-lock-add-keywords
+     nil
+     `((ami-font-lock-generics-matcher
+        (1 font-lock-type-face t)        ;; non-generic or outer generic type
+        (2 '(:foreground "rosybrown") t) ;; intermediate generic types
+        (3 '(:foreground "dark khaki") t) ;; inner generic type
+        (4 font-lock-variable-name-face t))
+       (,(ami-avoider-matcher-factory constant avoid-faces)
+        (0 font-lock-constant-face t))
+       (,(ami-avoider-matcher-factory annotation avoid-faces)
+        (0 font-lock-doc-face t))
+       (,(ami-avoider-matcher-factory "\\<enum\\>" avoid-faces)
+        (0 font-lock-keyword-face t))
+       )
+     t) ;; append to keywords list instead of prepending
+    ))
+
+(defun ami-avoider-matcher-factory (regexp avoid-faces)
+  "Return a matcher for REGEXP which avoids AVOID-FACES.
+The matcher is a function of one argument (limit) suitable for
+use as a MATCHER in `font-lock-keywords' that will match
+occurences of REGEXP which are not already fontified with any of
+AVOID-FACES."
+  `(lambda (limit)
+    ,(concat "Factory-generated matcher for matching `" regexp
+            "' while avoiding occurences already fontified with any of "
+            (prin1-to-string avoid-faces))
+    (if (not (search-forward-regexp ,regexp limit t))
+        nil
+      (if (not (member (get-text-property (match-beginning 0) 'face)
+                           (quote ,avoid-faces)))
+          t
+        ;; if already doc/comment/string move point along
+        (progn (goto-char (match-end 0))
+               (set-match-data (list limit limit limit limit limit
+                                     limit limit limit limit limit))
+               t)))))
+;; eval this to see an example of what the above generates
+;; (prin1-to-string (ami-avoider-matcher-factory "hello" '(f g h)))
+
+(defun ami-font-lock-generics-matcher (limit)
+  "Font-lock matcher function for Java 1.5 generics.
+The `match-data' set by this
+matcher will represent three matched subexpressions (any of which may be
+  empty) corresponding to the base type, any intermediate-level types, and the
+  inner-most types.  Only returns matches following point.  Moves point to the
+  end of the last subexpression.  Doesn't search beyond LIMIT."
+  (if (not (search-forward-regexp
+            "[^@]\\(\\_<[[:upper:]][A-Za-z0-9$_]*\\_>\\)" limit t))
+      nil
+    ;; TODO: this duplicates logic from ami-avoider-matcher-factory but I'm too
+    ;; tired to refactor this to use that right now.
+    (if (or (not (member (get-text-property (match-beginning 1) 'face)
+                         '(font-lock-comment-face
+                           font-lock-doc-face
+                           font-lock-string-face)))
+            ;; if already doc/comment/string move point along
+            (progn (goto-char (match-end 1)) nil))
+        (let* ((b (match-beginning 1))
+               (e (match-end 1))
+               (low-word "\\_<[[:lower:]][A-Za-z0-9$_]*\\_>")
+               (ret (list b e))
+               (prev-openers (count-matches "<"
+                                            (line-beginning-position) (point)))
+               (prev-closers (count-matches ">"
+                                            (line-beginning-position) (point)))
+               (outer (= prev-openers prev-closers))
+               (inner (and (search-forward-regexp "\\([<,>]\\)" limit t)
+                           (or (equal ">" (match-string 1))
+                               (equal "," (match-string 1)))))
+               (varb (or (and (goto-char e)
+                              ;; want to use looking-at but must obey limit!
+                              (search-forward-regexp
+                               (concat "[][> \t]*\\s-*\\(?:\\.\\.\\.\\)?\\s-*"
+                                       "\\(" low-word "\\(?:\\s-*,\\s-*"
+                                       low-word "\\)*\\)[^(]")
+                               limit t)
+                              (= e (match-beginning 0)) ;; imitate looking-at
+                              (match-beginning 1))
+                         0))
+               (vare (or (and (> varb 0) (match-end 1))
+                         0)))
+          ;;(message (format "outer=%s, inner=%s, b=%d, e=%d, varb=%d, vare=%d"
+          ;;                 outer inner b e varb vare))
+          (if outer
+              (setq ret (append ret (list b e 0 0 0 0)))
+            (if inner
+                (setq ret (append ret (list 0 0 0 0 b e)))
+              (setq ret (append ret (list 0 0 b e 0 0)))))
+          (set-match-data (append ret (list varb vare (current-buffer))))
+          (goto-char (max e vare)) ;; point may have moved since initial search
+          t)
+      ;; must not return nil from this method to allow artificial point movement
+      ;; caused by doc/comment/string detection to be considered.
+      (set-match-data (list limit limit limit limit limit
+                            limit limit limit limit limit))
+      t)))
+
+;;;###autoload(add-hook 'jde-mode-hook 'ami-jde-font-lock-java5)
+;;;###autoload(add-hook 'java-mode-hook 'ami-jde-font-lock-java5)
+
+(provide 'rc/ami-jde-font-lock-java5)
+
+;;; java5-font-lock.el ends here
